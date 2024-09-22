fbaa2af9ddd42f0d4af35f6d44318c4ff94a00bb
Medido1 <medi1222@gmail.com>
2024-09-22 22:49:31 +0100

    Complete mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '../assets/images/logo.svg';
 import hamburgerIcon from '../assets/images/icon-hamburger.svg';
 import closeIcon from '../assets/images/icon-close.svg';
+import introImg from '../assets/images/image-intro-mobile.jpg'
 import { useState } from 'react';
 import MobileMenu from './MobileMenu';
 
@@ -11,14 +12,17 @@ function Header() {
     setIsShowMenu(!isShowMenu);
   }
   return (
-    <header className="flex justify-between px-6 py-8">
-    <img src={logo} alt='insure logo' className='w-20 h-6'/>
-    <button onClick={showMenu} >
-      <img src={isShowMenu ? closeIcon : hamburgerIcon} alt="toggle menu" />
-    </button>
-    {isShowMenu && 
+    <header>
+      <div className='flex justify-between items-center p-4'>
+      <img src={logo} alt='insure logo' className='w-20 h-6'/>
+      <button onClick={showMenu} >
+        <img src={isShowMenu ? closeIcon : hamburgerIcon} alt="toggle menu" />
+      </button>
+      {isShowMenu && 
       <MobileMenu />
     }
+      </div>
+      <img src={introImg} alt="family members holding hands" />
   </header>
   )
   
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,8 +2,9 @@
 function MobileMenu() {
   return (
     <div 
-      className="w-screen h-screen  pt-10 absolute top-20 left-0 flex flex-col items-center text-center
-        bg-VeryDarkViolet
+      className="w-screen h-screen pt-10 absolute top-16 left-0 
+      flex flex-col items-center text-center
+      bg-VeryDarkViolet z-10
       ">
       <ul className="text-white text-lg font-bold flex flex-col gap-8">
         <li>HOW WE WORK</li>
